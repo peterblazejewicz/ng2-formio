6c8faa37ed8f958ffe646d4eb0c25c26b710e914
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-01-07 21:11:31 +0100

    Use Webpack html-loader for inlining html templates
    This removes need of using Gulp dedicated plugin to
    replace content in TypeScript source files

--- src/templates/bootstrap.tpl.ts
+++ src/templates/bootstrap.tpl.ts
@@ -8,13 +8,18 @@ import { AlignDirective } from '../components/signature/signature';
 import { SignaturePadModule } from 'angular2-signaturepad';
 
 let requiredCSS: string = '.required .control-label::after { content:" *"; color:red; }';
-let getTemplate = function(template: string) {
-    return {component: {template: template, styles: [requiredCSS]}};
+let getTemplate = (template: string ) => {
+    return {
+        component: {
+            template: template, 
+            styles: [requiredCSS]
+        }
+    };
 };
 export const FORMIO_BOOTSTRAP: FormioTemplate = {
     formio: {
         component: {
-            template: { gulp_inject: './bootstrap/formio.html' },
+            template: require('html!./bootstrap/formio.html'),
             styles: [
                 '.glyphicon-spin { -webkit-animation: spin-anim 1s infinite; animation: spin-anim 1s infinite; }',
                 '@-webkit-keyframes spin-anim {0% { -webkit-transform: rotate(0deg);transform: rotate(0deg);}100% { -webkit-transform: rotate(359deg);transform: rotate(359deg);}}',
@@ -24,7 +29,7 @@ export const FORMIO_BOOTSTRAP: FormioTemplate = {
     },
     formio_wizard: {
         component: {
-            template:{ gulp_inject: './bootstrap/formio-wizard.html' },
+            template: require('html!./bootstrap/formio-wizard.html'),
             styles: [
                 '.bs-wizard {border-bottom: solid 1px #e0e0e0; padding: 0 0 10px 0;line-height: 1em;}',
                 '.bs-wizard > .bs-wizard-step {padding: 0; position: relative;}',
@@ -50,52 +55,52 @@ export const FORMIO_BOOTSTRAP: FormioTemplate = {
             ]
         }
     },
-    formio_component: getTemplate({ gulp_inject: './bootstrap/formio-component.html' }),
-    formio_components: getTemplate({ gulp_inject: './bootstrap/formio-components.html' }),
-    errors: getTemplate({ gulp_inject: './bootstrap/errors.html' }),
+    formio_component: getTemplate(require('html!./bootstrap/formio-component.html')),
+    formio_components: getTemplate(require('html!./bootstrap/formio-components.html')),
+    errors: getTemplate(require('html!./bootstrap/errors.html')),
     components: {
-        button: getTemplate({ gulp_inject: './bootstrap/components/button.html' }),
-        columns: getTemplate({ gulp_inject: './bootstrap/components/columns.html' }),
-        container: getTemplate({ gulp_inject: './bootstrap/components/container.html' }),
-        datagrid: getTemplate({ gulp_inject: './bootstrap/components/datagrid.html' }),
-        input: getTemplate({ gulp_inject: './bootstrap/components/input.html' }),
-        number: getTemplate({ gulp_inject: './bootstrap/components/number.html' }),
-        textarea: getTemplate({ gulp_inject: './bootstrap/components/textarea.html' }),
-        hidden: getTemplate({ gulp_inject: './bootstrap/components/hidden.html' }),
-        radio: getTemplate({ gulp_inject: './bootstrap/components/radio.html' }),
-        checkbox: getTemplate({ gulp_inject: './bootstrap/components/checkbox.html' }),
-        custom: getTemplate({ gulp_inject: './bootstrap/components/custom.html' }),
-        table: getTemplate({ gulp_inject: './bootstrap/components/table.html' }),
-        panel: getTemplate({ gulp_inject: './bootstrap/components/panel.html' }),
-        fieldset: getTemplate({ gulp_inject: './bootstrap/components/fieldset.html' }),
-        well: getTemplate({ gulp_inject: './bootstrap/components/well.html' }),
-        day: getTemplate({ gulp_inject: './bootstrap/components/day.html' }),
+        button: getTemplate(require('html!./bootstrap/components/button.html')),
+        columns: getTemplate(require('html!./bootstrap/components/columns.html')),
+        container: getTemplate(require('html!./bootstrap/components/container.html')),
+        datagrid: getTemplate(require('html!./bootstrap/components/datagrid.html')),
+        input: getTemplate(require('html!./bootstrap/components/input.html')),
+        number: getTemplate(require('html!./bootstrap/components/number.html')),
+        textarea: getTemplate(require('html!./bootstrap/components/textarea.html')),
+        hidden: getTemplate(require('html!./bootstrap/components/hidden.html')),
+        radio: getTemplate(require('html!./bootstrap/components/radio.html')),
+        checkbox: getTemplate(require('html!./bootstrap/components/checkbox.html')),
+        custom: getTemplate(require('html!./bootstrap/components/custom.html')),
+        table: getTemplate(require('html!./bootstrap/components/table.html')),
+        panel: getTemplate(require('html!./bootstrap/components/panel.html')),
+        fieldset: getTemplate(require('html!./bootstrap/components/fieldset.html')),
+        well: getTemplate(require('html!./bootstrap/components/well.html')),
+        day: getTemplate(require('html!./bootstrap/components/day.html')),
         datetime: {
             component: {
-                template:{ gulp_inject: './bootstrap/components/datetime.html' },
+                template: require('html!./bootstrap/components/datetime.html'),
                 styles: [requiredCSS, ".showDate {width:48vmin;border:1px solid #ccc;}", ".showTime {width:53vmin;height:22vmin;border:1px solid #ccc;}", ".buttonsSpace {padding:5px;}"]
             },
             module: {
                 imports: [DatepickerModule, TimepickerModule]
             }
         },
-        selectboxes: getTemplate({ gulp_inject: './bootstrap/components/selectboxes.html' }),
-        content: getTemplate({ gulp_inject: './bootstrap/components/content.html' }),
-        html: getTemplate({ gulp_inject: './bootstrap/components/html.html' }),
-        currency: getTemplate({ gulp_inject: './bootstrap/components/currency.html' }),
+        selectboxes: getTemplate(require('html!./bootstrap/components/selectboxes.html')),
+        content: getTemplate(require('html!./bootstrap/components/content.html')),
+        html: getTemplate(require('html!./bootstrap/components/html.html')),
+        currency: getTemplate(require('html!./bootstrap/components/currency.html')),
         select: {
             component: {
-                template: { gulp_inject: './bootstrap/components/select.html' },
+                template: require('html!./bootstrap/components/select.html'),
                 styles: [requiredCSS]
             },
             module: {
                 imports: [SelectModule]
             }
         },
-        survey: getTemplate({ gulp_inject: './bootstrap/components/survey.html' }),
+        survey: getTemplate(require('html!./bootstrap/components/survey.html')),
         resource: {
             component: {
-                template: { gulp_inject: './bootstrap/components/resource.html' },
+                template: require('html!./bootstrap/components/resource.html'),
                 styles: [requiredCSS]
             },
             module: {
@@ -104,7 +109,7 @@ export const FORMIO_BOOTSTRAP: FormioTemplate = {
         },
         address: {
             component: {
-                template: { gulp_inject: './bootstrap/components/address.html' },
+                template: require('html!./bootstrap/components/address.html'),
                 styles: [requiredCSS]
             },
             module: {
@@ -113,7 +118,7 @@ export const FORMIO_BOOTSTRAP: FormioTemplate = {
         },
         phoneNumber: {
             component: {
-                template: {gulp_inject: './bootstrap/components/phonenumber.html'},
+                template: require('html!./bootstrap/components/phonenumber.html'),
                 styles: [requiredCSS]
             },
             module: {
@@ -122,7 +127,7 @@ export const FORMIO_BOOTSTRAP: FormioTemplate = {
         },
         signature: {
             component: {
-                template: { gulp_inject: './bootstrap/components/signature.html' },
+                template: require('html!./bootstrap/components/signature.html'),
                 styles: ['.required .footer::after { content:" *"; color:red; }', ".clearButton {position:absolute; left: 0; top: 0; z-index: 1000}", ".footer {text-align: center; color:#C3C3C3;}"]
             },
             module: {
